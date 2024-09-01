416bf4a6a6b833ae6831f7b87917a3e22c226086
yasin <yasinmiapolash@gmail.com>
2024-09-02 01:52:38 +0600

    update in network caller

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,48 +1,99 @@
-
 import 'dart:convert';
 import 'dart:developer';
-import 'package:http/http.dart' as http;
-class NetworkCaller{
+import 'package:http/http.dart';
+import '../models/response_data.dart';
+
+class NetworkCaller {
   //get methods
 
-  Future<dynamic> getMethod(String url)async{
+  Future<ResponseData> getRequest(String url) async {
+    log(url);
+    try {
+      final Response response = await get(Uri.parse(url),
+          headers: {'Content-type': 'application/json'});
+      log(response.statusCode.toString());
+      log(response.body.toString());
+      if (response.statusCode == 200) {
+        final decodedResponse = jsonDecode(response.body);
+        if (decodedResponse['status'] == 'success') {
+          return ResponseData(
+              isSuccess: true,
+              statusCode: response.statusCode,
+              responseData: decodedResponse);
+        } else {
+          return ResponseData(
+              isSuccess: false,
+              statusCode: response.statusCode,
+              responseData: decodedResponse,
+              errorMessage: decodedResponse['data'] ?? 'Something went wrong');
+        }
+      } else if (response.statusCode == 401) {
+        return ResponseData(isSuccess: false, statusCode: response.statusCode, responseData: '');
+      }else{
+        return ResponseData(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          responseData: '',
+        );
 
-    try{
-      final http.Response response= await http.get(Uri.parse(url),headers:{
-        'Content-type':'application/json'
-      } );
-
-      if(response.statusCode==200){
-        return jsonDecode(response.body);
-      }else if(response.statusCode==401){
-        log('Error');
       }
-      
-    }catch(e){
-     log("error: $e");
+    } catch (e) {
+      log(" Error: $e");
+      return ResponseData(
+        isSuccess: false,
+        statusCode: 500,
+        responseData: '',
+        errorMessage: 'Server error occurred',
+      );
     }
   }
 
+  Future<ResponseData> postRequest(String url, {Map<String, String>? body}) async {
+    log(url);
+    log(body.toString());
 
-  Future<dynamic> postMethod(String url ,{Map<String,String>? body}) async {
+    try {
+      final Response response = await post(Uri.parse(url),
+          headers: {'Content-type': 'application/json'},
+          body: jsonEncode(body));
+      log(response.statusCode.toString());
+      log(response.body.toString());
 
+      if (response.statusCode == 200) {
+        final decodedResponse=jsonDecode(response.body);
+        if(decodedResponse['status'] == 'success'){
+          return ResponseData(isSuccess: true, statusCode: response.statusCode, responseData: response.body);
 
-    
-    try{
-      final http.Response response=await http.post(Uri.parse(url),headers: {
-        'Content-type':'application/json'
-        
-      },body: jsonEncode(body));
-      
-      if(response.statusCode==200){
-        return jsonDecode(response.body);
-      }else if(response.statusCode==401){
-        log('Error: UnAuthorize');
+        }else{
+          return ResponseData(
+            isSuccess: false,
+            statusCode: response.statusCode,
+            responseData: decodedResponse,
+            errorMessage: decodedResponse['data'] ?? 'Something went wrong',
+          );
+        }
+
+      } else if (response.statusCode == 401) {
+        return ResponseData(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          responseData: '',
+        );
+      }else{
+        return ResponseData(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          responseData: '',
+        );
       }
-    }catch(e){
+    } catch (e) {
       log('Error: $e');
+      return ResponseData(
+        isSuccess: false,
+        statusCode: 400,
+        responseData: '',
+        errorMessage: 'An error occurred: $e',
+      );
     }
   }
-
 }
-
