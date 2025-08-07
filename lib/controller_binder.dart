2799bab0dadab090270eb87128c3e30e12190e65
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-08-07 12:45:29 +0600

    clean up ControllerBinder and update dependencies in pubspec.lock

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,11 +1,8 @@
-
 import 'package:get/get.dart';
-import 'package:network_caller/controllers/product_list.dart';
 
-class ControllerBinder extends Bindings{
+class ControllerBinder extends Bindings {
   @override
   void dependencies() {
-   // Get.put(ProductListController());
+    // Get.put(ProductListController());
   }
-
 }
