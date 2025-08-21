b1509b878a1d3dda9500f7ac9d390f58456efcf1
Yasin Mia palash <yasinmiapolash@gmail.com>
2025-08-21 15:18:28 +0600

    Implement course and session management with API integration

--- lib/Core/utils/constants/app_urls.dart
+++ lib/Core/utils/constants/app_urls.dart
@@ -3,5 +3,7 @@ class AppUrls {
 
   static const String baseUrl = 'https://dummyjson.com/';
   static const String login = '${baseUrl}auth/login';
+  //courses
+  static const String course = 'https://employee-beryl.vercel.app/api/v1/students/courses-session';
   static const String lookupAddress = 'google.com';
 }
